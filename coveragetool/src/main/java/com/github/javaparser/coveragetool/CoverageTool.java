e72b6697f60b907e6ff320e0a83c68c71c95d25e
Albin <albinso@kth.se>
2018-02-27 00:29:23 +0100

    feat: Working code coverage tool. Might require being added as dependency before being imported into some modules.

--- coveragetool/src/main/java/com/github/javaparser/coveragetool/CoverageTool.java
+++ coveragetool/src/main/java/com/github/javaparser/coveragetool/CoverageTool.java
@@ -1,11 +1,39 @@
 package com.github.javaparser.coveragetool;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class CoverageTool {
     private static final HashMap<String, Boolean> coverage = new HashMap<>();
+    private static final String outFile = "coverage.out";
 
-    public void makeCovered(String id) {
+    static {
+        System.out.println("Setting shutdown hook");
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override public void run() {
+                try {
+                    System.out.println("Running shutdown hook");
+                    writeToFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void makeCovered(String id) {
+        System.out.println(id);
         coverage.put(id, true);
+    }
+
+    public static void writeToFile() throws IOException {
+        String[] keys = new String[coverage.size()];
+        keys = coverage.keySet().toArray(keys);
+        FileWriter writer = new FileWriter(outFile);
+        for(String str: keys) {
+            writer.write(str);
+        }
+        writer.close();
     }
 }
